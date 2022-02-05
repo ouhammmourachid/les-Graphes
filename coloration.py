852505c1d8058d2353b523f10c39906ea4bdede4
ouhammmourachid <93659459+ouhammmourachid@users.noreply.github.com>
2022-02-05 23:01:19 +0100

    Update coloration.py

--- coloration.py
+++ coloration.py
@@ -1,24 +1,23 @@
 import numpy as np
 def coloration(M):
     pour_ordonee=[sum(i) for i in M]
-    M=np.array(M)[np.argsort(pour_ordonee)[::-1]]
+    B= [ i for i in np.argsort(pour_ordonee)[::-1] ]
+    print(B)
     k=0
-    B=[i for i in range(len(M))]
-    deja_colorer=[]
+    ordoner_ordre_croisant = B.copy()
+    deja_colorer = []
     while B!=[]:
         k+=1
-        colorer_par_k=[]
-        while B!=[]:
-            colorer_par_k=[]
-            colorer_par_k+=[B[0]]
-            deja_colorer+=[B[0]]
+        colorer_par_k = []
+        while B != []:
+            colorer_par_k += [B[0]]
+            deja_colorer += [B[0]]
             del B[0]
             C=[]
             for i in B:
-                for j in colorer_par_k:
-                    if M[i,j]!=0:
-                        C+=[i]
+                if sum([ M[i][j] for j in colorer_par_k ]) == 0:
+                    C += [i]
             B=C[:]
-        B=[i for i in range(len(M)) if i not in deja_colorer]
+        print(colorer_par_k)
+        B=[i for i in ordoner_ordre_croisant if i not in deja_colorer]
     return k
-
